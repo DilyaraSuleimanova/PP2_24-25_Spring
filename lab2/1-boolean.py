af10056c186f5a99ab6866fcc08c4c2d86f6a3b1
DilyaraSuleimanova <suleimanovadilyara04@gmail.com>
2025-01-20 10:09:47 +0500

    First commit

---
+++ lab2/1-boolean.py
@@ -0,0 +1,17 @@
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+# FUNCTION
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+
+# OBJECT IS OF A CERTAIN DATA TYPE
+x = 200
+print(isinstance(x, int))
