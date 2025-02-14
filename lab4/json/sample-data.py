b93eeb0062894a0734a4b4206706b033b7a04cbb
DilyaraSuleimanova <suleimanovadilyara04@gmail.com>
2025-02-14 16:22:06 +0500

    update

--- lab4/json/sample-data.py
+++ lab4/json/sample-data.py
@@ -1,7 +1,14 @@
 import json
 
-json_file = 'data.json'
+with open('C:\\Users\\User\\Desktop\\materials\\PP\\PP2\\labs\\lab4\\json\\data.json', 'r') as f:
+    data = json.load(f)
 
-with open(json_file, 'r') as f:
-    js = json.load(f)
-    print(js)
+print("Interface Status")
+print(80 * "=")
+print("DN" + 40 * ' ', "Description", "Speed ", "  MTU", sep = "\t")
+print(43 * '-', 13 * '-', 7 * '-', 7 * '-', sep = '\t')
+for item in data["imdata"]:
+    print(item["l1PhysIf"]["attributes"]["dn"], end = "\t")
+    if item["l1PhysIf"]["attributes"]["descr"] == "":
+        print(" " * len("Description"), end = "\t")
+    print(item["l1PhysIf"]["attributes"]["speed"], item["l1PhysIf"]["attributes"]["mtu"], sep = "   ")
