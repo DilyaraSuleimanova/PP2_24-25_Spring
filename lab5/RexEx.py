a31634df4a4c575d11ac12f8937285deccb4049f
DilyaraSuleimanova <suleimanovadilyara04@gmail.com>
2025-02-19 23:31:39 +0500

    First commit

---
+++ lab5/RexEx.py
@@ -0,0 +1,65 @@
+import re
+# # 1 task 
+
+# str = input("Enter string: ")
+# pattern = "ab*"
+# result = re.findall(pattern, str)
+# print(result)
+
+
+# # 2 task
+# str = input("Enter string: ")
+# pattern = "ab{2,3}"
+# result = re.findall(pattern, str)
+# print(result)
+
+
+# # 3 task
+# str = input("Enter string: ")
+# pattern = "[a-z]*_[a-z]*"
+# result = re.findall(pattern, str)
+# print(result)
+
+
+# # 4 task
+# str = input("Enter string: ")
+# pattern = "[A-Z][a-z]+"
+# result = re.findall(pattern, str)
+# print(result)
+
+
+# # 5 task
+# str = input("Enter string: ")
+# pattern = r"a\w*b\b"
+# result = re.findall(pattern, str)
+# print(result)
+
+
+# # 6 task
+# str = input("Enter string: ")
+# pattern = r"[\s,.:]"
+# repl = ''
+# result = re.sub(pattern, repl, str)
+# print(result)
+
+
+# # 7 task
+# str = input("Enter string: ")
+# def repl(m):
+#     return m.group(1).upper()
+
+# result = re.sub(r"_([a-z])", repl, str)
+# print(result)
+
+
+# # 8 task
+# str = input("Enter string: ")
+# result = re.split(r"[A-Z]", str)
+# print(result)
+
+
+# # 9 task
+# str = input("Enter string: ")
+# result = re.sub(r"([a-z])([A-Z])", r"\1 \2", str)
+# print(result)
+
